f51e0dadabcf93a1cecd5ed6d2d3365b6be67113
Vladislav <v-vbronzov@microsoft.com>
2021-06-15 13:19:32 +0300

    added docker components

---
+++ api/backends.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.backends import ModelBackend
+
+from .models import CustomUser
+
+
+class EmailBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, email=None, **kwargs):
+        UserModel = CustomUser
+        try:
+            user = UserModel.objects.get(email=kwargs['email'])
+        except UserModel.DoesNotExist:
+            return None
+        else:
+            if user.check_password(password):
+                return user
+        return None
